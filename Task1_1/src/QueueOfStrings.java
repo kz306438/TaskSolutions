af9f37b3a3eb5fa0109d4c572af5f97406d8ea96
Kirill Zhuravlev <kz306438@gmail.com>
2024-10-14 01:59:56 +0300

    Task1_1

---
+++ Task1_1/src/QueueOfStrings.java
@@ -0,0 +1,28 @@
+/**
+ * @brief Interface for a queue of strings.
+ */
+public interface QueueOfStrings {
+    /**
+     * @brief Adds a string to the end of the queue.
+     * @param str The string to be added.
+     */
+    void enqueue(String str);
+
+    /**
+     * @brief Removes and returns the string from the front of the queue.
+     * @return The string at the front of the queue.
+     */
+    String dequeue();
+
+    /**
+     * @brief Checks if the queue is empty.
+     * @return True if the queue is empty, otherwise false.
+     */
+    boolean isEmpty();
+
+    /**
+     * @brief Returns the number of elements in the queue.
+     * @return The current size of the queue.
+     */
+    int size();
+}
